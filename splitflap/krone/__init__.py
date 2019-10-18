6c57f7082d7ef3b0f8a9c02ec41908d11a85ccf8
Julian Metzler <git@mezgr.de>
2019-10-18 20:27:12 +0200

    Renamed Krone module files

--- splitflap/krone/__init__.py
+++ splitflap/krone/__init__.py
@@ -15,5 +15,5 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from .krone_fbm import KroneFBMController
-from .krone_hlst import KroneHLSTController
+from .fbm import KroneFBMController
+from .hlst import KroneHLSTController
